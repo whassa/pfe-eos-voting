4c6a59ad50e304ad2c0ab9eb38e4d7b73b7f7fc9
whassa <remi.graton@gmail.com>
2022-02-23 14:47:01 -0500

    Add styling for the chat

--- src/component/LiveChat/LiveChat.js
+++ src/component/LiveChat/LiveChat.js
@@ -1,69 +1,60 @@
-
 import { useEffect, useState } from "react";
 import GUN from "gun/gun";
 import SEA from "gun/sea";
 import LiveChatMessage from "./LiveChatMessage";
 
-import { Grid, Paper, Container } from "@mui/material";
+import { Grid, Paper, FormControl, Box, Input, Button } from "@mui/material";
 
-let messages = [];
-const gun =  GUN();
-const user = gun.user().recall({sessionStorage: true});
+const gun = GUN();
 
 const match = {
   // lexical queries are kind of like a limited RegEx or Glob.
-  '.': {
+  ".": {
     // property selector
-    '>': new Date(+new Date() - 1 * 1000 * 60 * 60 * 3).toISOString(), // find any indexed property larger ~3 hours ago
+    ">": new Date(+new Date() - 1 * 1000 * 60 * 60 * 3).toISOString(), // find any indexed property larger ~3 hours ago
   },
-  '-': 1, // filter in reverse
+  "-": 1, // filter in reverse
 };
 
-
 export default function LiveChat({ ual, resolution, encryptionKey }) {
-
-  let [newMessage, setNewMessage] = useState('');
+  let [newMessage, setNewMessage] = useState("");
   let [messages, setMessages] = useState([]);
-  
 
   async function sendMessage() {
     const index = new Date().toISOString();
     // new TextDecoder().decode(uin8arry)
     const publicKey = ual.activeUser.session.publicKey.data.array;
     const messageToEncrypt = {
-      message:newMessage,
-      userName:ual.activeUser.accountName,
-      userPublicKey:publicKey
+      message: newMessage,
+      userName: ual.activeUser.accountName,
+      userPublicKey: publicKey,
     };
     const secret = await SEA.encrypt(messageToEncrypt, encryptionKey);
     // const message = user.get('all').set({ what: secret });
-    gun.get("liveChat:" + resolution.id)
-      .put({ [index]: secret});
-    console.log('message sent');
+    gun.get("liveChat:" + resolution.id).put({ [index]: secret });
     setNewMessage("");
   }
 
-  useEffect( async () => {
-  
-    gun.get("liveChat:" + resolution.id)
+  useEffect(async () => {
+    gun
+      .get("liveChat:" + resolution.id)
       .map(match)
       .once(async (data, id) => {
-        console.log(data);
         if (data) {
           // Key for end-to-end encryption
-          const messageDecrypted = (await SEA.decrypt(data, encryptionKey))
+          const messageDecrypted = await SEA.decrypt(data, encryptionKey);
           var message = {
             // transform the data
             publicKey: messageDecrypted.userPublicKey,
             who: messageDecrypted.userName,
-            what: messageDecrypted.message + '', // force decrypt as text.
+            what: messageDecrypted.message + "", // force decrypt as text.
             when: id, // get the internal timestamp for the what property.
           };
           if (message.what) {
-            setMessages(oldMessages => [...oldMessages, message] );
+            setMessages((oldMessages) => [...oldMessages, message]);
           }
         }
-      })
+      });
   }, []);
 
   return (
@@ -73,63 +64,61 @@ export default function LiveChat({ ual, resolution, encryptionKey }) {
       direction="column"
       alignItems="center"
       justify="center"
-      style={{ minHeight: "100vh", padding: "16px", marginTop: "70px" }}
+      style={{ marginTop: "20px" }}
     >
-      <Container maxWidth="sm">
-        <Paper elevation={3} padding="dense">
-          {ual.activeUser ? (
-            <Grid>
-              <main>
-                {messages.map((message) => {
-                  console.log(message.when);
-                  return (
-                    <LiveChatMessage
-                      key={message.when}
-                      message={message}
-                      sender={ual.activeUser.accountName}
-                    />
-                  );
-                })}
-              </main>
-              <form
-                onSubmit={(e) => {
-                  e.preventDefault();
-                  sendMessage();
-                }}
-              >
-                <input
-                  type="text"
-                  placeholder="Type a message..."
-                  onChange={(e) => {setNewMessage(e.target.value)}}
-                  value={newMessage}
-                />
-                <button type="submit" disabled={!newMessage}>
-                  Submit
-                </button>
-              </form>
-            </Grid>
-          ) : (
-            <Grid>
-              <main>
-                {messages.map((message) => {
-                  console.log(message.when);
-                  return (
-                    <LiveChatMessage
-                      key={message.when}
-                      message={message}
-                      sender={ual.activeUser.accountName}
-                    />
-                  );
-                })}
-              </main>
-              <Grid>
-                You are not connected. Please connect to your EOS/EDEN account
-                in order to use the live chat.
-              </Grid>
-            </Grid>
-          )}
-        </Paper>
-      </Container>
+      <Paper
+        elevation={3}
+        padding="dense"
+        sx={{
+          height: "600px",
+          width: "100%",
+          display: "flex",
+          flexDirection: "column",
+          justifyContent: "space-between",
+        }}
+      >
+        <Box sx={{overflowY: 'scroll', margin: '10px', height: '100%' }}>
+
+
+
+
+          {messages.map((message) => {
+            return (
+              <LiveChatMessage
+                ual={ual}
+                key={message.when}
+                message={message}
+              />
+            );
+          })}
+        </Box>
+        {ual.activeUser ? (
+          <FormControl
+            sx={{ margin: '10px', display: 'flex', flexDirection: 'row'}}
+          >
+            <Input
+              type="text"
+              placeholder="Type a message..."
+              onChange={(e) => {
+                setNewMessage(e.target.value);
+              }}
+              value={newMessage}
+              sx={{width: '100%', marginRight: '5px'}}
+            />
+            <Button  onClick={(e) => {
+              e.preventDefault();
+              sendMessage();
+            }} type="submit" variant="contained" >
+              Submit
+            </Button>
+          </FormControl>
+        ) : (
+          <Box sx={{ margin: '10px', display: 'flex', flexDirection: 'row'}}>
+            You are not connected. Please connect to your EOS/EDEN account in
+            order to use the live chat.
+          </Box>
+        )}
+      </Paper>
     </Grid>
   );
 }
