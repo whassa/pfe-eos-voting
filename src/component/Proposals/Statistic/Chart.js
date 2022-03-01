d92df71a6d59ce2f3a3ce060327502b36e80bc50
Anthony Vargas-Gobeille <anthony.vargas-gobeille.1@ens.etsmtl.ca>
2022-02-28 23:38:51 -0500

    chart in statistic done

---
+++ src/component/Proposals/Statistic/Chart.js
@@ -0,0 +1,120 @@
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+} from "chart.js";
+import { Line } from "react-chartjs-2";
+import moment from "moment";
+
+ChartJS.register(
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend
+);
+
+function valueOverTime(votesList, voteType) {
+    let voteData = {
+      x: [],
+      y: [],
+    };
+    let nbrOfVotes = 0;
+    votesList.forEach((vote) => {
+      if (vote.value === voteType) {
+        nbrOfVotes++;
+        const newVote = {
+          x: moment(vote.updatedAt).valueOf(),
+          y: nbrOfVotes,
+        };
+        voteData.x.push(newVote.x);
+        voteData.y.push(newVote.y);
+      }
+    });
+    const lastVote = votesList[votesList.length - 1];
+    if (lastVote && lastVote?.value !== voteType) {
+      voteData.x.push(moment(lastVote.updatedAt).valueOf());
+      voteData.y.push(nbrOfVotes);
+    }
+    return voteData;
+  }
+
+export default function Chart({ dataVotes }) {
+    //Chart options and data
+    const options = {
+        responsive: true,
+        plugins: {
+            title: {
+                display: true,
+                text: "Vote Tracker",
+                align: "start",
+                font: {
+                    size: 14
+                }
+            },
+        },
+        layout: {
+            padding: 5
+        },
+        scales: {
+            y: {
+                ticks: {
+                    stepSize: 1
+                }
+            }
+        }
+    };
+
+    const labels = [
+        "January",
+        "February",
+        "March",
+        "April",
+        "May",
+        "June",
+        "July",
+        "August",
+        "September",
+        "October",
+        "November",
+        "December",
+    ];
+
+    const data = {
+        labels,
+        datasets: [
+            {
+                label: "Yes",
+                data: valueOverTime(dataVotes, true).y,
+                fill: false,
+                borderColor: 'rgb(0, 255, 0)'
+              },
+              {
+                label: "No",
+                data: valueOverTime(dataVotes, false).y,
+                fill: false,
+                borderColor: 'rgb(255, 0, 0)'
+              },
+              {
+                label: "Refrain",
+                data: valueOverTime(dataVotes, null).y,
+                fill: false,
+                borderColor: 'rgb(0, 0, 255)'
+              }
+        ],
+    };
+
+    return (
+        <Line
+            data={data}
+            options={options}
+        />
+    );
+}
--- src/component/Proposals/Statistic/Statistic.js
+++ src/component/Proposals/Statistic/Statistic.js
@@ -1,22 +1,20 @@
-import {
-    Grid,
-    Paper,
-    Stack,
-    Typography,
-    Box,
-} from "@mui/material";
+import { Grid, Paper, Stack, Typography, Box } from "@mui/material";
 import { useTheme } from "@mui/styles";
+import Chart from "./Chart";
 
 export default function Statistics({ resolution }) {
     const theme = useTheme();
     let prosVotes = 0;
     let consVotes = 0;
+    let refrainVotes = 0;
 
     resolution &&
         resolution.votes &&
         resolution.votes.items &&
         resolution.votes.items.forEach((vote) => {
-            if (vote.value) {
+            if (vote.value == null) {
+                refrainVotes++;
+            } else if (vote.value) {
                 prosVotes++;
             } else {
                 consVotes++;
@@ -24,46 +22,86 @@ export default function Statistics({ resolution }) {
         });
 
     return (
-        <Box sx={{ marginTop: theme.homeMarginTop }} >
-            <Stack direction="row" spacing={2} >
-                <Paper elevation={3} padding="dense" sx={{ padding: '10px', flex: 1 }}>
+        <Box sx={{ marginTop: theme.homeMarginTop }}>
+            <Stack direction="row" spacing={2}>
+                <Paper
+                    elevation={3}
+                    padding="dense"
+                    sx={{ padding: "10px", flex: 1 }}
+                >
                     <Typography>Total votes</Typography>
-                    <Typography sx={{ textAlign: 'center', color: theme.palette.colors.coolSage, fontSize: '64px'}}>{prosVotes - consVotes}</Typography>
-                    <Stack direction="row" spacing={2} sx={{ justifyContent: 'center'}}>
+                    <Typography
+                        sx={{
+                            textAlign: "center",
+                            color: theme.palette.colors.coolSage,
+                            fontSize: "64px",
+                        }}
+                    >
+                        {prosVotes - consVotes}
+                    </Typography>
+                    <Stack
+                        direction="row"
+                        spacing={2}
+                        sx={{ justifyContent: "center" }}
+                    >
                         <Stack>
-                            <Typography>
-                                Yes
-                            </Typography>
-                            <Typography sx={{textAlign: 'center'}}>
+                            <Typography>Yes</Typography>
+                            <Typography sx={{ textAlign: "center" }}>
                                 {prosVotes}
                             </Typography>
                         </Stack>
                         <Stack>
-                            <Typography>
-                                No
-                            </Typography>
-                            <Typography sx={{textAlign: 'center'}}>
+                            <Typography>No</Typography>
+                            <Typography sx={{ textAlign: "center" }}>
                                 {consVotes}
+                            </Typography>
+                        </Stack>
+                        <Stack>
+                            <Typography>No</Typography>
+                            <Typography sx={{ textAlign: "center" }}>
+                                {refrainVotes}
                             </Typography>
                         </Stack>
                     </Stack>
                 </Paper>
 
-                <Paper elevation={3} padding="dense"  sx={{ padding: '10px', flex: 1 }}>
+                <Paper
+                    elevation={3}
+                    padding="dense"
+                    sx={{ padding: "10px", flex: 1 }}
+                >
                     <Typography>Vote margin</Typography>
-                    <Typography>???</Typography>
-                    <Typography>???</Typography>
+                    <Typography
+                        sx={{
+                            textAlign: "center",
+                            color: theme.palette.colors.coolSage,
+                            fontSize: "64px",
+                        }}
+                    >
+                        ???
+                    </Typography>
+                    <Typography sx={{ textAlign: "center" }}>???</Typography>
                 </Paper>
 
-                <Paper elevation={3} padding="dense"  sx={{ padding: '10px', flex: 1 }}>
+                <Paper
+                    elevation={3}
+                    padding="dense"
+                    sx={{ padding: "10px", flex: 1 }}
+                >
                     <Typography>Remaining days</Typography>
-                    <Typography>
+                    <Typography
+                        sx={{
+                            textAlign: "center",
+                            color: theme.palette.colors.coolSage,
+                            fontSize: "64px",
+                        }}
+                    >
                         {Math.ceil(
                             Math.abs(resolution.expireAt._d - Date.now()) /
                                 (1000 * 60 * 60 * 24)
                         )}
                     </Typography>
-                    <Typography>
+                    <Typography sx={{ textAlign: "center" }}>
                         {resolution.expireAt._d.toLocaleString("default", {
                             month: "long",
                         })}{" "}
@@ -73,9 +111,8 @@ export default function Statistics({ resolution }) {
                 </Paper>
             </Stack>
             <Box sx={{ marginTop: theme.homeMarginTop }}>
-                <Paper elevation={3} padding="dense" >
-                    <Typography>This is a chart</Typography>
-                    <Typography>STONK</Typography>
+                <Paper elevation={3} padding="dense">
+                    <Chart dataVotes={resolution.votes.items} />
                 </Paper>
             </Box>
         </Box>
