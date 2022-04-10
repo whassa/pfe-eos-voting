25a1ac9ba1b605f4a677c40c8fc9110fe58d43ff
whassa <remi.graton@gmail.com>
2022-04-10 11:03:45 -0400

    Fix referencing

--- src/component/Head/Header.js
+++ src/component/Head/Header.js
@@ -3,7 +3,7 @@ export default function Header() {
   return (
     <Head>
         <title>Eos Proposals</title>
-        <meta name="description" content="Eos Proposals is a voting platform using the blockchain eos. It's purpose is to show have a hub where users can vote on different subject." />
+        <meta name="description" content="Eos Proposals is a voting platform using the blockchain eos. It's purpose is to show votes and be a hub where users can vote on different subject." />
         <link rel="icon" href="/favicon.ico" />
     </Head>
   );
